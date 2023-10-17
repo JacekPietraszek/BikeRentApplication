8bebc32fa7569d5f93c7353396e92be276426add
Jacek Pietraszek <jacekpietraszek01@gmail.com>
2023-10-17 21:02:45 +0200

    Initial commit

---
+++ src/main/java/com/bikerent/bikerentapplication/BikeRentApplication.java
@@ -0,0 +1,13 @@
+package com.bikerent.bikerentapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikeRentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BikeRentApplication.class, args);
+    }
+
+}
